da5393a2d11faf16d06e491771adf01df5de3e6b
BeomSeok Ko <love@BeomSeoks-MacBook-Pro.local>
2016-08-07 19:40:20 -0400

    1

--- lotto.c
+++ lotto.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-<<<<<<< HEAD
-=======
 // mainline commens.. but hotfix3
->>>>>>> hotfix3
 void    calling_new_feature ( void )
 {
         printf("new feature .. \n");
