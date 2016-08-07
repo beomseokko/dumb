db7237411a52762fc9bdbff6684037588169e053
BeomSeok Ko <love@BeomSeoks-MacBook-Pro.local>
2016-08-07 17:22:23 -0400

    adding calling_lotto_function

--- lotto.c
+++ lotto.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+void    calling_lotto_function (void)
+{
+
+        for ( int i = 0 ; i < 10 ; i++)
+        {
+            printf("[%2d] = %d\n", random());
+        }
+}
 int main(int argc, char *argv[])
 {
         printf("Lotto main.. \n");
+        calling_lotto_function();
         return 0;
 }
